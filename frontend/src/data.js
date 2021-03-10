6f1811a54506209e0f94b13d9a0c8c227035ca87
hafizwasi <hafizwasi99@gmail.com>
2021-03-10 11:12:53 -0800

    Commit message

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data={
+    products:[
+        {
+            _id: '1',
+            name: 'Bread',
+            image: '/images/bread.jpeg',
+            price: '120',
+            rating: '4.5',
+            numReviews: '10',
+
+        },
+        {
+            _id: '2',
+            name: 'Cake',
+            image: '/images/cake.jpg',
+            price: '400',
+            rating: '4.5',
+            numReviews: '20',
+
+        },
+        {
+            _id: '3',
+            name: 'Jam',
+            image: '/images/jam.jpg',
+            price: '250',
+            rating: '4.7',
+            numReviews: '7',
+
+        },
+        {
+            _id: '4',
+            name: 'Buscuits',
+            image: '/images/buscuit.jpg',
+            price: '200',
+            rating: '4.5',
+            numReviews: '15',
+
+        },
+        {
+            _id: '5',
+            name: 'Chocklates',
+            image: '/images/chocklate.jpg',
+            price: '120',
+            rating: '4.5',
+            numReviews: '10',
+
+        },
+        {
+            _id: '6',
+            name: 'Butter',
+            image: '/images/butter.jpg',
+            price: '50',
+            rating: '4.0',
+            numReviews: '10',
+
+        },
+        {
+            _id: '7',
+            name: 'Bun',
+            image: '/images/bun.jpg',
+            price: '40',
+            rating: '4.5',
+            numReviews: '10',
+
+        },
+        {
+            _id: '8',
+            name: 'Pizza',
+            image: '/images/pizza.jpg',
+            price: '400',
+            rating: '4.3',
+            numReviews: '25',
+
+        },
+    ],
+};
+export default data;
