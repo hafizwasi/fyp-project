a24a8641e6d35e22593d4ed4d6b1f8b16f67d11a
hafizwasi <hafizwasi99@gmail.com>
2021-03-15 05:24:04 -0700

    Node and express server created

---
+++ backend/data.js
@@ -0,0 +1,95 @@
+const data={
+    products:[
+        {
+            _id: '1',
+            name: 'Bread',
+            image: '/images/bread.jpeg',
+            price: '120',
+            countInStock: 10,
+            rating: '5.0',
+            numReviews: '10',
+
+        },
+        {
+            _id: '2',
+            name: 'Cake',
+            image: '/images/cake.jpg',
+            price: '400',
+            countInStock: 20,
+            rating: '4.5',
+            numReviews: '20',
+
+        },
+        {
+            _id: '3',
+            name: 'Jam',
+            image: '/images/jam.jpg',
+            price: '250',
+            countInStock: 5,
+            rating: '4.8',
+            numReviews: '7',
+
+        },
+        {
+            _id: '4',
+            name: 'Buscuits',
+            image: '/images/buscuit.jpg',
+            price: '200',
+            countInStock: 100,
+            rating: '4.5',
+            numReviews: '15',
+
+        },
+        {
+            _id: '5',
+            name: 'Chocklates',
+            image: '/images/chocklate.jpg',
+            price: '120',
+            countInStock: 40,
+            rating: '4.5',
+            numReviews: '10',
+
+        },
+        {
+            _id: '6',
+            name: 'Butter',
+            image: '/images/butter.jpg',
+            price: '50',
+            countInStock: 0,
+            rating: '4.0',
+            numReviews: '10',
+
+        },
+        {
+            _id: '7',
+            name: 'Bun',
+            image: '/images/bun.jpg',
+            price: '40',
+            countInStock: 20,
+            rating: '4.5',
+            numReviews: '10',
+
+        },
+        {
+            _id: '8',
+            name: 'Pizza',
+            image: '/images/pizza.jpg',
+            price: '400',
+            countInStock: 0,
+            rating: '4.3',
+            numReviews: '25',
+
+        },
+        {
+            _id: '9',
+            name: 'Egg',
+            image: '/images/egg.jpg',
+            price: '150',
+            countInStock: 30,
+            rating: '5',
+            numReviews: '25',
+
+        },
+    ],
+};
+export default data;
