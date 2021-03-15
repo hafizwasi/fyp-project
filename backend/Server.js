adccd87495d4e068d71d9324f1d16bf95eb8ae84
hafizwasi <hafizwasi99@gmail.com>
2021-03-15 08:40:49 -0700

    nodemon successfully added

--- backend/Server.js
+++ backend/Server.js
@@ -1,17 +1,16 @@
 import express from 'express';
 import data from './data.js';
 
-
 const app = express();
 
 app.get('/api/products', (req, res) => {
-    res.send(data.products);
-    });
-
-app.get('/', (req, res) => {
-res.send('server is ready');
+  res.send(data.products);
 });
 
-app.listen(5000, ()=>{
-console.log('serve at http://localhost:5000');
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
