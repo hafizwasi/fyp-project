f284b672aa9db3595719de881b4ea7321bb0717e
hafizwasi <hafizwasi99@gmail.com>
2021-04-06 16:43:50 +0500

    Shipping Address screen implement

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,6 +15,9 @@ import { userRegisterReducer, userSigninReducer } from './reducers/userReducers'
       cartItems: localStorage.getItem('cartItems')
         ? JSON.parse(localStorage.getItem('cartItems'))
         : [],
+        shippingAddress: localStorage.getItem('shippingAddress')
+        ? JSON.parse(localStorage.getItem('shippingAddress'))
+        : {},
     },
   };
 const reducer = combineReducers({
